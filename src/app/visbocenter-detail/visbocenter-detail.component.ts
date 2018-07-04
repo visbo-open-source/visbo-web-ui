9449f833591483eb9792568c0b7107783a093283
Markus Seyfried <Markus@seyfried.bayern>
2018-07-04 07:29:06 +0200

    Improved error handling for User Management in VC

--- src/app/visbocenter-detail/visbocenter-detail.component.ts
+++ src/app/visbocenter-detail/visbocenter-detail.component.ts
@@ -3,7 +3,7 @@ import { Location } from '@angular/common';
 //import { ActivatedRoute } from '@angular/router';
 import { ActivatedRoute, Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-
+import { AlertService } from '../_services/alert.service';
 import { MessageService } from '../_services/message.service';
 import { VisboCenter } from '../_models/visbocenter';
 import { VCUser } from '../_models/visbocenter';
@@ -25,7 +25,8 @@ export class VisboCenterDetailComponent implements OnInit {
     private visboprojectService: VisboProjectService,
     private route: ActivatedRoute,
     private location: Location,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) { }
 
   ngOnInit() {
@@ -66,17 +67,36 @@ export class VisboCenterDetailComponent implements OnInit {
     message = message.trim();
     this.messageService.add(`Add VisboCenter User: ${email} Role: ${role} VC: ${vcid}`);
     if (!email || !role) { return; }
-    this.visbocenterService.addVCUser({ email: email, role: role, message: message} as VCUser, vcid )
-      .subscribe(users => { this.visbocenter.users.push(users[0]); });
+    this.visbocenterService.addVCUser({ email: email, role: role} as VCUser, message, vcid )
+      .subscribe(
+        users => {
+          this.visbocenter.users.push(users[0]);
+          this.alertService.success(`User ${email} added successfully`);
+        },
+        error => {
+          this.messageService.add(`Add VisboCenter User error: ${error.error.message}`);
+          this.alertService.error(error.error.message);
+        }
+
+      );
       // .subscribe(users => { users; });
       // show up alter about success / error
   }
 
   removevcuser(user: VCUser, vcid: string): void {
     this.messageService.add(`Remove VisboCenter User: ${user.email}/${user.userId} Role: ${user.role} VC: ${vcid}`);
-    // MS TODO filter user after the ReST Call depending on result.
-    this.visbocenter.users = this.visbocenter.users.filter(vcUser => (vcUser.email !== user.email || vcUser.role !== user.role));
-    this.visbocenterService.deleteVCUser(user, vcid ).subscribe();
+    this.visbocenterService.deleteVCUser(user, vcid)
+      .subscribe(
+        result => {
+          // this.messageService.add(`Remove VisboCenter User result: ${JSON.stringify(result)}`);
+          this.visbocenter.users = result;
+          this.alertService.success(`User ${user.email} removed successfully`);
+        },
+        error => {
+          this.messageService.add(`Remove VisboCenter User error: ${error.error.message}`);
+          this.alertService.error(error.error.message);
+        }
+      );
   }
 
 }
