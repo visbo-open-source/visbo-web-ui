a7dc7cf84bd1aa9f3fd97b83b032da78582e6e51
Markus <markus@seyfried.bayern>
2018-09-19 09:35:34 +0200

    Log Files

---
+++ src/app/_services/syslogs.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+import { VisboFile, VisboFilesResponse, VisboDownloadResponse } from '../_models/visbofiles';
+
+import { MessageService } from './message.service';
+import { LoginComponent } from '../login/login.component';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class SysLogsService {
+
+  private serviceUrl = environment.restUrl.concat('/syslogs');  // URL to ReST api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+
+  /** GET List of Logs from the server */
+  getSysLogs(): Observable<VisboFile[]> {
+    var url = this.serviceUrl
+
+    this.log(`Calling HTTP Request: ${url} `);
+    return this.http.get<VisboFilesResponse>(url, httpOptions)
+      .pipe(
+        map(response => response.files),
+        tap(files => this.log(`fetched ${files.length} Log Files `)),
+        catchError(this.handleError('getSysLogs', []))
+      );
+  }
+
+  // /** GET Log File by name. Return 403 when name not found */
+  getSysLog(name: string): Observable<any> {
+    var url = `${this.serviceUrl}/${name}`;
+    this.log(`Calling HTTP Request for a specific log file: ${url}`);
+    return this.http.get<VisboDownloadResponse>(url)
+      .pipe(
+        map(response => this.log(`fetched Log File Response ${JSON.stringify(response)}`)),
+        // tap(stream => this.log(`fetched Log File`)),
+        catchError(this.handleError<VisboFile>(`getSysLog name:${name} `))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      this.log(`HTTP Request failed: ${JSON.stringify(error)} status:${error.status}`);
+
+      // user no longer authenticated, remove it from the session
+      if (error.status == 401) {
+        this.log(`${operation} failed: ${error.message}`);
+        sessionStorage.removeItem('currentUser');
+      }
+      // Let the app keep running by returning an empty result.
+      return new ErrorObservable(error);
+    };
+  }
+
+  /** Log a VisboLogsService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('VisboLogsService: ' + message);
+  }
+}
