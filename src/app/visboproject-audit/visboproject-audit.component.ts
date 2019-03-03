2063098382c2062f1349bfaf07af0804ff3aa065
Markus <markus@seyfried.bayern>
2019-03-03 17:15:11 +0100

    string number issue

--- src/app/visboproject-audit/visboproject-audit.component.ts
+++ src/app/visboproject-audit/visboproject-audit.component.ts
@@ -236,18 +236,17 @@ export class VisboProjectAuditComponent implements OnInit {
   }
 
   helperResponseText(visboaudit: VisboAudit): string {
-    if (!visboaudit.result) return "Unknown"
     if (visboaudit.result.statusText) return visboaudit.result.statusText;
     var status = visboaudit.result.status
-    if (status == 200) return "Success"
-    if (status == 304) return "Success"
-    if (status == 400) return "Bad Request"
-    if (status == 401) return "Not Authenticated"
-    if (status == 403) return "Permission Denied"
-    if (status == 404) return "URL not found"
-    if (status == 409) return "Conflict"
-    if (status == 423) return "Locked"
-    if (status == 500) return "Server Error"
+    if (status == "200") return "Success"
+    if (status == "304") return "Success"
+    if (status == "400") return "Bad Request"
+    if (status == "401") return "Not Authenticated"
+    if (status == "403") return "Permission Denied"
+    if (status == "404") return "URL not found"
+    if (status == "409") return "Conflict"
+    if (status == "423") return "Locked"
+    if (status == "500") return "Server Error"
     return status.toString()
   }
 
