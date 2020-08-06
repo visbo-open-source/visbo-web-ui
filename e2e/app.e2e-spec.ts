5a28a8429f24f6fc4039dab2c37ab42d25257a93
Markus <markus@seyfried.bayern>
2020-08-06 20:46:05 +0200

    e2e status before migration

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,39 +4,50 @@ import { VisboPage } from './app.po';
 describe('VISBO Visual Board', () => {
   let page: VisboPage;
 
-  beforeEach(() => {
-    page = new VisboPage();
+  beforeEach(async () => {
+    console.log("Create new Visbo Login Page");
+    page = await new VisboPage();
+    // browser.waitForAngular();
   });
 
-  it('should display correct title', () => {
-    browser.ignoreSynchronization = true;
-    browser.get('/');
-    browser.driver.getTitle()
-      .then(function(pageTitle) {
-        expect(pageTitle).toEqual('VISBO: Visual Board');
-      });
-    // page.navigateTo();
-    // console.log("Navigated to VisboPage", page || 'undefined');
-    //
-    // let title = page.getHeadingText();
-    // console.log("Heading ");
-    // let tempTitle = 'Welcome to the VISBO - the project warehouse'
-    // expect(tempTitle).toEqual('Welcome to the VISBO - the project warehouse');
-  });
-
-  it('should display username and password field', async () => {
-    var loginName = await element(by.id('LoginName')).getText();
-    // var loginName = await element(by.css('app-login')).getText();
-    console.log("LoginName ", loginName)
-    // expect(text).not.toEqual(undefined);
-    // var loginPW = browser.findElement(by.id('LoginPW1'));
-    // text = loginPW.getText();
-    // console.log("LoginPW ", text != undefined)
-    // expect(text).not.toEqual(undefined);
-    // element(by.id('LoginName')).getText()
-    //   .then(function(text) {
-    //     console.log("got LoginName");
-    //     // expect(text).toEqual('');
+  it('should display correct title', async () => {
+    // browser.ignoreSynchronization = true;
+    // browser.get('/');
+    // browser.driver.getTitle()
+    //   .then(function(pageTitle) {
+    //     expect(pageTitle).toEqual('VISBO: Visual Board');
     //   });
+    await page.navigateTo();
+    console.log("NavigateTo finished");
+    let pageTitle = await browser.driver.getTitle();
+    expect(pageTitle).toEqual('VISBO: Visual Board');
   });
+
+  it('should show missing input error', async () => {
+    // browser.ignoreSynchronization = true;
+    await page.navigateTo();
+    console.log("Set loginName");
+    await page.userName("aaaaaaaaaa");
+
+    console.log("Check getName loginName");
+    expect(page.getName('loginName')).toBe(true);
+    // var loginNameError = await element(by.id('loginNameError')).getText() || '';
+    // var loginPWError = await element(by.id('loginPWError')).getText() || '';
+    //
+    // expect(loginNameError.length).toBeGreaterThan(0);
+    // expect(loginPWError.length).toBeGreaterThan(0);
+  });
+
+  it('should display login error message', async () => {
+    // browser.ignoreSynchronization = true;
+    await page.navigateTo();
+    console.log("LoginName with wrong username & password");
+    await page.login("abc@visbo.de", "abc");
+    // expect(await page.loaded()).toBe(true);
+    // var alertMessage = await element(by.id('alertMessage')).getText() || '';
+
+    // expect(alertMessage.length).toBeGreaterThan(0);
+    console.log("LoginName with wrong username & password done");
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,17 +1,41 @@
 import { browser, by, element } from 'protractor';
 
 export class VisboPage {
-  navigateTo() {
-    // return browser.get('/');
-    browser.get('/')
-      .then(() => { console.log("Start Page Loaded..."); browser.getTitle(); })
-      .then(title => console.log("Title: ", title));
+  loginName = element(by.id('loginName'));
+  loginPW = element(by.id('loginPW'));
+  loginButtonLogin = element(by.id('loginButtonLogin'));
+  loginButtonRegister = element(by.id('loginButtonRegister'));
+  loginButtonPWForgotten = element(by.id('loginButtonPWForgotten'));
 
-    // browser.debugger();
+  navigateTo = async function() {
+    // return browser.get('/');
+    // console.log("Start Page Loading...");
+    await browser.get('/');
+    console.log("Start Page Loaded");
   }
 
-  getHeadingText() {
-    console.log("get Heading Text");
-    return element(by.css('app-root h2')).getText();
+  getName = async function(name: string) {
+    console.log("Get Value by nameID for:", name);
+    var result = await element(by.id(name)).getText();
+    console.log("Get Value by nameID Result:", result);
+    return result;
+  }
+
+  login = async function(email: string, password: string) {
+    console.log("Login User:", email);
+    await this.loginName.sendKeys(email);
+    console.log("Login Username set");
+    await this.loginPW.sendKeys(password);
+    console.log("Login Password set");
+    await this.loginButtonLogin.click();
+    console.log("Login Button clicked");
+  }
+
+  userName = async function(email: string) {
+    console.log("Enter Login User:", email);
+    await element(by.id('loginName')).sendKeys(email);
+    console.log("UserName entered");
+    await element(by.id('loginButtonLogin')).click();
+    console.log("Button Clicked");
   }
 }
