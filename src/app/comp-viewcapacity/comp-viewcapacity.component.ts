decc9b4d859cfd5ced15d29040296a2233896192
Markus Seyfried <markus.seyfried@visbo.de>
2021-05-28 07:51:29 +0200

    excel export with links to vp/vpf/vc

--- src/app/comp-viewcapacity/comp-viewcapacity.component.ts
+++ src/app/comp-viewcapacity/comp-viewcapacity.component.ts
@@ -21,7 +21,7 @@ import { VisboSettingService } from '../_services/visbosetting.service';
 
 import { VGPermission, VGPVC, VGPVP } from '../_models/visbogroup';
 
-import { getErrorMessage, visboCmpDate, convertDate, validateDate, visboCmpString, visboIsToday, getPreView }
+import { getErrorMessage, visboCmpDate, convertDate, validateDate, visboCmpString, visboIsToday, getPreView, visboGetShortText }
             from '../_helpers/visbo.helper';
 
 import { scale, brewer } from 'chroma-js';
@@ -1723,53 +1723,83 @@ export class VisboCompViewCapacityComponent implements OnInit, OnChanges {
     return result;
   }
 
+  copyCapacity(vpv: VisboCapacity, name: string): VisboCapacity {
+    let copy: VisboCapacity = Object.assign({}, vpv);
+    copy.month = new Date(vpv.month);
+    copy.name = name;
+    delete copy.vpid;
+
+    return copy;
+  }
+
   exportExcel(): void {
     this.log(`Export Data to Excel ${this.visboCapacity?.length} ${this.visboCapacityChild?.length}`);
     // convert list to matix
 
     const excel: VisboCapacity[] = [];
 
+    let name = '';
+    let urlWeb = ''
+    let listURL: string[] = [];
+    const tooltip = this.translate.instant('ViewCapacity.msg.viewWeb');
+    if (this.vpfActive) {
+      name = this.vpfActive.name
+      urlWeb = window.location.origin.concat('/vpf/', this.vpfActive.vpid, '?view=Capacity');
+    } else if (this.vpActive) {
+      name = this.vpActive.name;
+      urlWeb = window.location.origin.concat('/vpKeyMetrics/', this.vpActive._id, '?view=Capacity');
+    } else if (this.vcActive) {
+      name = this.vcActive.name;
+      urlWeb = window.location.origin.concat('/vp/', this.vcActive._id, '?view=KeyMetrics&viewCockpit=Capacity');
+    }
     if (this.visboCapacity) {
       this.visboCapacity.forEach(element => {
-        element.month = new Date(element.month);
-        excel.push(element);
+        excel.push(this.copyCapacity(element, name));
+        listURL.push(urlWeb);
       });
     }
     if (this.visboCapacityChild) {
       this.visboCapacityChild.forEach(element => {
-        element.month = new Date(element.month);
-        excel.push(element);
+        let urlWebDetail = urlWeb;
+        if (element.name) {
+          urlWebDetail = window.location.origin.concat('/vpKeyMetrics/', element.vpid, '?view=Capacity');
+        }
+        excel.push(this.copyCapacity(element, element.name || name));
+        listURL.push(urlWebDetail);
       });
     }
 
     const len = excel.length;
     const width = Object.keys(excel[0]).length;
-    const matrix = 'A1:' + XLSX.utils.encode_cell({r: len, c: width});
-    let name = '';
-    if (this.vpfActive) {
-      name = this.vpfActive.name
-    } else if (this.vpActive) {
-      name = this.vpActive.name;
-    } else if (this.vcActive) {
-      name = this.vcActive.name;
-    }
     this.log(`Export Data to Excel ${excel.length}`);
     // Add Localised header to excel
     // eslint-disable-next-line
     const header: any = {};
+    let colName: number, colIndex = 0;
     for (const element in excel[0]) {
       this.log(`Processing Header ${element}`);
+      if (element == 'name') {
+        colName = colIndex;
+      }
+      colIndex++;
       header[element] = this.translate.instant('ViewCapacity.lbl.'.concat(element))
     }
     excel.unshift(header);
     this.log(`Header for Excel: ${JSON.stringify(header)}`)
 
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(excel, {skipHeader: true});
+    for (let index = 1; index <= len; index++) {
+      const address = XLSX.utils.encode_cell({r: index, c: colName});
+      const url = listURL[index - 1];
+      worksheet[address].l = { Target: url, Tooltip: tooltip };
+    }
+    const matrix = 'A1:' + XLSX.utils.encode_cell({r: len, c: width});
     worksheet['!autofilter'] = { ref: matrix };
     // eslint-disable-next-line
     const sheets: any = {};
-    sheets[name] = worksheet;
-    const workbook: XLSX.WorkBook = { Sheets: sheets, SheetNames: [name] };
+    const sheetName = visboGetShortText(name, 30);
+    sheets[sheetName] = worksheet;
+    const workbook: XLSX.WorkBook = { Sheets: sheets, SheetNames: [sheetName] };
     // eslint-disable-next-line
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     const actDate = new Date();
