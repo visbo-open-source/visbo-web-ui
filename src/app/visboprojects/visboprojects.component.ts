3339c1a7688d19f8c36163cbb1672f12fb79320a
UteRK <ute.rittinghaus-koytek@visbo.de>
2024-12-16 16:39:02 +0100

    only from a project with vpType=0 a new VISBO Project can be created

--- src/app/visboprojects/visboprojects.component.ts
+++ src/app/visboprojects/visboprojects.component.ts
@@ -45,6 +45,7 @@ export class VisboProjectsComponent implements OnInit {
   visboprojects: VisboProject[];
   visboprojectsAll: VisboProject[];
   vpTemplates: VisboProject[];
+  vpTempProjects: VisboProject[];
   vcSelected: string;
   vcActive: VisboCenter;
   newVP: CreateProjectProperty;
@@ -194,6 +195,7 @@ export class VisboProjectsComponent implements OnInit {
           this.visboprojectsAll = visboprojects;
           this.updateVPProperties();
           this.filterVP();
+          this.initProjectlist(visboprojects);
           this.initTemplates(visboprojects);
           this.initDropDown();
           this.switchView();
@@ -721,6 +723,18 @@ export class VisboProjectsComponent implements OnInit {
       this.vpTemplates.push(vp);
     }
   }
+
+  initProjectlist(vps: VisboProject[]): void {
+    this.vpTempProjects = vps.filter(item => item.vpType == 0);
+    if (this.vpTempProjects.length > 0) {
+      const vp = new VisboProject();
+      delete vp._id;
+      vp.name = this.translate.instant('vp.lbl.noTemplate');
+      this.vpTempProjects.push(vp);
+    }
+  }
+
+
 
   getCommitDate(vp: VisboProject):Date {
     if (!vp) { return undefined }
