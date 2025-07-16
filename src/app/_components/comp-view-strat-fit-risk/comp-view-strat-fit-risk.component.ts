dd632457ad9c97e93533a1fa8f99e141354ae45f
UteRK <ute.rittinghaus-koytek@visbo.de>
2025-07-16 09:41:52 +0200

    changed the criterium for the color of the bubble in strategicFit-risk-chart

--- src/app/_components/comp-view-strat-fit-risk/comp-view-strat-fit-risk.component.ts
+++ src/app/_components/comp-view-strat-fit-risk/comp-view-strat-fit-risk.component.ts
@@ -931,9 +931,9 @@ export class CompViewStratFitRiskComponent implements OnInit, OnChanges {
 
   getProfitCategory(profit: number): number {    
     if (profit) {
-      if (profit < 0) return 0;
-      if (profit == 0) return 1;
-      if (profit > 0) return 2;
+      if (profit < -0.01) return 0;
+      if (profit > 0.01) return 2;
+      return 1;
     } else {
       return 1;
     }
